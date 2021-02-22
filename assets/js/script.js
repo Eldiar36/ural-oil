cae6f71abf5ad47ae5acce9f5aa578ab344ba76c
eldiar36 <eldiarrd@gnail.com>
2021-02-22 19:11:59 +0600

    edit2

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,28 @@ function burgerMenu(selector) {
     }
 }
 burgerMenu('.burger-menu');
+function burgerMenuPc(selector) {
+    let menu = $(selector);
+    let button = menu.find(".burger-pc-menu-button");
+    let links = menu.find(".pc-link");
+    let overlay = menu.find("burger-pc-menu_overlay");
+    button.on("click",(e) => {
+        e.preventDefault();
+        toogleMenu();
+    });
+    links.on("click", () => toogleMenu());
+    overlay.on("click", () => toogleMenu());
+    function toogleMenu() {
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css("overflow","hidden");
+        }else  {
+            $("body").css("overflow","visible")
+        }
+    }
+}
+burgerMenuPc('.burger-pc-menu');
 $('.nav_menu  li').on('click', function () {
     $(this).addClass('active')
         .siblings().removeClass('active');
